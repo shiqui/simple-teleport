58ad49041c439b752ce3bfb299c6646f0466372e
shiqui <shiqi8sq@gmail.com>
2023-11-25 22:37:40 -0500

    refactor(command): clean up sethome

--- src/main/java/me/shiqui/simpleteleport/commands/SetHomeCommand.java
+++ src/main/java/me/shiqui/simpleteleport/commands/SetHomeCommand.java
@@ -1,6 +1,5 @@
 package me.shiqui.simpleteleport.commands;
 
-import me.shiqui.simpleteleport.SimpleTeleport;
 import me.shiqui.simpleteleport.utils.DatabaseHelper;
 import me.shiqui.simpleteleport.utils.MessageHelper;
 import org.bukkit.Location;
@@ -10,33 +9,33 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.Objects;
-import java.util.UUID;
 
 public class SetHomeCommand implements CommandExecutor {
-
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender == null || command == null || label == null) { return true; }
+        if (!(sender instanceof Player)) { return true; }
+        Player player = (Player)sender;
+        Location location = player.getLocation();
 
-        if (sender instanceof Player){
+        DatabaseHelper.insertHome(
+            player.getUniqueId(),
+            Objects.requireNonNull(location.getWorld()).getName(),
+            location.getX(),
+            location.getY(),
+            location.getZ(),
+            location.getYaw(),
+            location.getPitch()
+        );
 
-            Player player = (Player)sender;
-            Location location = player.getLocation();
-            UUID uuid = player.getUniqueId();
+        player.sendMessage(
+            MessageHelper
+                .stringFromConfig("sethome.msg")
+                .replace("<x>", String.valueOf(Math.round(location.getX())))
+                .replace("<y>", String.valueOf(Math.round(location.getY())))
+                .replace("<z>", String.valueOf(Math.round(location.getZ())))
+        );
 
-            DatabaseHelper.insertHome(uuid, location.getWorld().getName(), location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
-
-            String x = String.valueOf(Math.round(location.getX()));
-            String y = String.valueOf(Math.round(location.getY()));
-            String z = String.valueOf(Math.round(location.getZ()));
-
-            String msg = MessageHelper.stringFromConfig("sethome.msg")
-                .replace("<x>", x)
-                .replace("<y>", y)
-                .replace("<z>", z);
-
-            player.sendMessage(msg);
-
-        }
         return true;
 
     }
